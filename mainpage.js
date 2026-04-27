80745778b08b0c594123e05b7c948cefefb5e313
gyuraydeniz <gyuraydeniz@gmail.com>
2026-04-27 19:08:53 +0200

    skills update

--- mainpage.js
+++ mainpage.js
@@ -1,5 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
-    
     const yearElement = document.getElementById('current-year');
     if (yearElement) {
         yearElement.textContent = new Date().getFullYear();
